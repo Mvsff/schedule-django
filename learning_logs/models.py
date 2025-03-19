fd2b80b38d2e268fe4ee6d4db581f582a302cbd0
Mvsff <marcus.filho.mama@gmail.com>
2025-03-19 20:49:52 -0300

    tabela criada e adicionada ao banco de dados

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    """
+    Um assunto sobre o qual o usuário está aprendendo
+    (Comentários é uma boa prática de programação)
+    """
+    text = models.CharField(max_length=255)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.text
+    #Vai obter a data e a hora do sistema, e inserir junto ao 'text'.
+    
