0cacf54cd23a22130ad407d945ae0c3771cd12f2
Marcus Vinícius <marcus.viniciussff.dev@gmail.com>
2025-03-31 15:44:02 -0300

    Adicionada primeira url

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -12,4 +12,31 @@ class Topic(models.Model):
     def __str__(self):
         return self.text
     #Vai obter a data e a hora do sistema, e inserir junto ao 'text'.
-    
+
+
+class Entry(models.Model):
+    """
+    Adiciona uma descrição sob o tópico que foi criado
+    """
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    #Chave estrangeira, para relacionar a classe "Topic" com o atributo "topic"
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """
+        Classe criada APENAS para correção de erro de linguagem do Django,
+        quando o Django quiser usar Entry no plural, por padrão ele só
+        adiciona um "S" ao final do nome da classe, neste caso ficaria Entrys.
+        Ou seja, é unicamente para ajustar esse erro.
+        Não é obrigatório, apenas uma boa prática.
+        """
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return self.text[:50] + '...'
+
+    #Retorna apenas os 50 primeiros caracteres de text.
+
+   
+        
