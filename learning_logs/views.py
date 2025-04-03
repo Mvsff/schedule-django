47746144058690b17ca9cd8376810d0a897f06b3
Marcus Vinícius <marcus.viniciussff.dev@gmail.com>
2025-04-03 17:01:28 -0300

    Implementando mais uma rota, configurando o views, ainda incompleto

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
+from .models import Topic
 def index(request):
     '''
     Função da página principal do learning_log
     '''
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """
+    Mostra todos os assuntos/tópicos do site
+    """
+    topic = Topic.objects.order_by
+    """
+    Em resumo, vou pegar todo o objeto que está no models Topic
+    (tudo que está dentro do banco de dados de Topic), e eu quero ordenar para
+    
+    """
